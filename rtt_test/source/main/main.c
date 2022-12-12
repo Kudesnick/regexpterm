465a7d9b9b33831cd8b946d2e90da155113cc44d
Stulov Tikhon <kudesnick@inbox.ru>
2022-12-12 23:28:09 +0300

    [mod] Модифицировал тест для эмуляции строки приветствия

--- rtt_test/source/main/main.c
+++ rtt_test/source/main/main.c
@@ -22,12 +22,14 @@ int main(void)
 {
     Buttons_Initialize();
 
-	while (Buttons_GetState())
+	do
 	{
 		for (int i = 0; i < 10; i++)
 			printf("\033[31mC\033[32mO\033[33mL\033[34mO\033[35mR\033[42m \033[0m\033[36mT\033[37mE\033[30m\033[47mS\033[0mT "); // Color test
 		printf("\n");
 	}
+	while (Buttons_GetState());
+	printf("Insert please_");
 
     for(;;)
     {
